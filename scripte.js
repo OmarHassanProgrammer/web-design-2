666cbca65d35c7e7f1873f23db1351d944fd9df8
futureProgrammer-Pro <omar13102005@gmail.com>
2019-11-24 13:20:04 +0200

    color the color balit after change the color

--- scripte.js
+++ scripte.js
@@ -2,21 +2,20 @@ var brush = document.getElementById("brush"),
     eraser = document.getElementById("eraser"),
     stats = "",
     table = document.getElementById("canvas"),
-    color = "";
+    color = color = document.querySelector(".panel span input").value;
+
 
 document.body.onmousedown = function () {
     if(stats != "clear") {
         stats = "clicked";
         color = document.querySelector(".panel span input").value;
     }
-    console.log(stats);
 }
 
 document.body.onmouseup = function () {
     if(stats != "clear") {
         stats = "";
     }
-    console.log(stats);
 
 }
 
@@ -25,7 +24,6 @@ eraser.onclick = function () {
         stats = "";
     else stats = "clear";
     eraser.classList.toggle("active");
-    console.log(stats);
 }
 
 var height = window.innerHeight / 5;
@@ -39,18 +37,24 @@ for(var i = 0; i <= height; i++) {
         var td = document.createElement("td");
         td.setAttribute("data-row", row);
         td.setAttribute("data-col", col);
-        td.setAttribute("onmouseover", "draw("+ 
-        
-        row + "," + col +")")
+        td.setAttribute("onmouseover", "draw("+ row + "," + col +")")
         tr.appendChild(td);
     }
 }
 
 function draw (row, col) {
+    color = color = document.querySelector(".panel span input").value;
+    colorBrush();
+
     var ele = document.querySelector("[data-row='" + row + "'][data-col='" + col + "']");
     if(stats == "clicked") {
         ele.setAttribute("style", "background-color: "+ color +"")
     } else if (stats == "clear") {
         ele.setAttribute("style", "background-color: #fff")
-    }
+    }   
 }
+
+function colorBrush () {
+    document.querySelector("#brush i").setAttribute("style", "background-color: "+ color );
+}
+colorBrush();
